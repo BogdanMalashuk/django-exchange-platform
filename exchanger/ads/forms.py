9cdf42b52ba728ef0b467628babdcc98d939ff18
BogdanMalashuk <kufar.120@gmail.com>
2025-07-15 15:26:41 +0300

    ad creating & editing form for template

---
+++ exchanger/ads/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Ad, ExchangeProposal
+
+
+class AdForm(forms.ModelForm):
+    class Meta:
+        model = Ad
+        fields = ['title', 'description', 'image_url', 'category', 'condition']
