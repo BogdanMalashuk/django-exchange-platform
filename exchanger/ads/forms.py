f447dd29b5fb3b2227f3eca3c45fd2e587122685
BogdanMalashuk <kufar.120@gmail.com>
2025-07-16 17:14:18 +0300

    exchange proposal form was added

--- exchanger/ads/forms.py
+++ exchanger/ads/forms.py
@@ -6,3 +6,14 @@ class AdForm(forms.ModelForm):
     class Meta:
         model = Ad
         fields = ['title', 'description', 'image_url', 'category', 'condition']
+
+
+class ExchangeProposalForm(forms.ModelForm):
+    class Meta:
+        model = ExchangeProposal
+        fields = ['ad_sender', 'comment']
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+        self.fields['ad_sender'].queryset = Ad.objects.filter(user=user)
