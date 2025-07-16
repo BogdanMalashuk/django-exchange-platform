db0cf4b26cd9196ed13abc685a5eb0caa395dc5e
BogdanMalashuk <kufar.120@gmail.com>
2025-07-16 20:52:45 +0300

    register form fields fixed, logout view added

--- exchanger/authorization/views.py
+++ exchanger/authorization/views.py
@@ -1,13 +1,26 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.views.decorators.http import require_POST
 
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
             return redirect('ad_list')
     else:
         form = UserCreationForm()
     return render(request, 'auth/register.html', {'form': form})
+
+
+@require_POST
+def logout_view(request):
+    logout(request)
+    return redirect('ad_list')
